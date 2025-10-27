239a6d83d78bec466762ece0c0475672a0e1510a
Kirishanth Pirathapan <ec23358@qmul.ac.uk>
2025-10-27 14:00:40 +0000

    just did the viva so some things were changed

--- Code/ECS639U_Web/djangotut/polls/urls.py
+++ Code/ECS639U_Web/djangotut/polls/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
-    path("<int:question_id>/result/", views.rslt, name="results"),
+    path("<int:question_id>/poll_results/", views.results_view, name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
